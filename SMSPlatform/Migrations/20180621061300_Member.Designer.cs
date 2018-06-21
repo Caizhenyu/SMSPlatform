121c29b189c7bb02ffa07161fff028b6e1cf2242
蔡振宇 <caizy@dliangzao.com>
2018-06-21 14:14:23 +0800

    Add Member

--- SMSPlatform/Migrations/20180621061300_Member.Designer.cs
+++ SMSPlatform/Migrations/20180621061300_Member.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SMSPlatform.Models;
 
 namespace SMSPlatform.Migrations
 {
     [DbContext(typeof(SMSContext))]
-    partial class SMSContextModelSnapshot : ModelSnapshot
+    [Migration("20180621061300_Member")]
+    partial class Member
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SMSPlatform/Migrations/20180621061300_Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMSPlatform.Migrations
+{
+    public partial class Member : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MemberNo = table.Column<string>(nullable: false),
+                    MemberName = table.Column<string>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Phone = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.MemberId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Member");
+        }
+    }
+}
